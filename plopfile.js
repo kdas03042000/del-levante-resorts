3b17dc5a0a86342041bc331720b2673b145b61d6
krishanu.das <krishanu.das@highradius.com>
2022-04-21 19:16:31 +0530

    Pushing advance plop js configurations and hbs files

--- plopfile.js
+++ plopfile.js
@@ -1,50 +1,103 @@
-
-const config = (plop) => {
-  plop.setGenerator('Component', {
-    description: 'Generating Component',
+const config = (
+  /** @type {import('plop').NodePlopAPI} */
+  plop,
+) => {
+  plop.setGenerator('Mother File Generator', {
+    description: 'To be used to create a component or a container',
     prompts: [
+      {
+        type: 'list',
+        name: 'folderType',
+        message: 'What would you like to create?',
+        choices: ['containers', 'components'],
+      },
       {
         type: 'input',
         name: 'folderName',
-        message: 'What is the name of your component?',
-      },
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: 'src/components/{{folderName}}/index.tsx',
-        templateFile: 'plop-templates/index.hbs',
+        message: 'Name of the folder: ',
       },
       {
-        type: 'add',
-        path: 'src/components/{{folderName}}/styles.ts',
-        templateFile: 'plop-templates/styles.hbs',
-      },
+        type: 'confirm',
+        name: 'memoize',
+        message: 'Do you want to memoize your React Node?',
+        default: true,
+      }
     ],
+    //   actions: [
+    //     {
+    //       type: 'add',
+    //       path: 'src/components/{{folderName}}/index.tsx',
+    //       templateFile: 'plop-templates/index.hbs',
+    //     },
+    //     {
+    //       type: '',
+    //       path: 'src/components/{{folderName}}/styles.ts',
+    //       templateFile: 'plop-templates/styles.hbs',
+    //     },
+    //   ],
+    // actions: (data) => {
+    //   let actions = [];
+    //   if (data.folderType === 'Container')
+    //     actions.push({
+    //       type: 'add',
+    //       path: `src/containers/{{pascalCase folderName}}/index.tsx`,
+    //       templateFile: 'plop-templates/index.hbs',
+    //     });
+    //   else {
+    //     actions.push({
+    //       type: 'add',
+    //       path: `src/components/{{pascalCase folderName}}/index.tsx`,
+    //       templateFile: 'plop-templates/index.hbs',
+    //     });
+    //   }
+    //   if (data.requireStyles) {
+    //     actions.push({
+    //       type: 'add',
+    //       path: `src/components/{{pascalCase folderName}}/index.tsx`,
+    //       templateFile: 'plop-templates/index.hbs',
+    //     });
+    //   }
+    // },
+    actions: (data) => {
+      let actions = [];
+      actions.push(
+        {
+          type: 'add',
+          path: `src/{{folderType}}/{{pascalCase folderName}}/index.tsx`,
+          templateFile: 'plop-templates/index.hbs',
+        },
+        {
+          type: 'add',
+          path: `src/{{folderType}}/{{pascalCase folderName}}/styles.ts`,
+          templateFile: 'plop-templates/styles.hbs',
+        },
+      );
+      return actions;
+    }
   });
 
-  plop.setGenerator('Container', {
-    description: 'Generating Container',
-    prompts: [
-      {
-        type: 'input',
-        name: 'folderName',
-        message: 'What is the name of your container?',
-      },
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: 'src/containers/{{folderName}}/index.tsx',
-        templateFile: 'plop-templates/index.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/containers/{{folderName}}/styles.ts',
-        templateFile: 'plop-templates/styles.hbs',
-      },
-    ],
-  });
+  // plop.setGenerator('Container', {
+  //   description: 'Generating Container',
+  //   prompts: [
+  //     {
+  //       type: 'input',
+  //       name: 'folderName',
+  //       message: 'What is the name of your container?',
+  //     },
+  //   ],
+  //   actions: [
+  //     {
+  //       type: 'add',
+  //       path: 'src/containers/{{folderName}}/index.tsx',
+  //       templateFile: 'plop-templates/index.hbs',
+  //     },
+  //     {
+  //       type: 'add',
+  //       path: 'src/containers/{{folderName}}/styles.ts',
+  //       templateFile: 'plop-templates/styles.hbs',
+  //     },
+  //   ],
+  // });
 };
 
 module.exports = config;
